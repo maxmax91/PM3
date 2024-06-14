bca06dfd5c9eb471f89da96dc1d9f071f5ea22d1
Massimiliano <47661870+maxmax91@users.noreply.github.com>
2024-06-14 09:14:40 +0000

    test_commands draft

---
+++ tests/test_commands.py
@@ -0,0 +1,38 @@
+# PM3 daemon start
+# PM3 daemon stop
+# PM3 daemon status
+
+# PM3 new ... -- create a new process with a low max_restarts
+# PM3 new ... -- create a new process printing to stderr
+# PM3 new ... -- create a new process printing to stdout
+
+# PM3 ls ALL --json
+# PM3 ls ALL --list
+# PM3 ls all
+# PM3 ps
+
+# PM3 start 1
+
+# PM3 stop 1
+
+# PM3 restart 1
+
+# PM3 reset 1
+
+# PM3 rm 1
+
+# PM3 rename 1
+
+# PM3 log 1
+
+# PM3 err 1
+
+# PM3 dump > file.json -- save as file
+
+# PM3 rm 1
+
+# PM3 load < file.json -- load from file
+
+# PM3 the process should be here again
+
+# PM3 make_script systemd
