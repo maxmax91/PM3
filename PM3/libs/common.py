c636f69f5d371b6edee2178d1b1b029d0a3237d0
Massimiliano <47661870+maxmax91@users.noreply.github.com>
2024-06-17 22:05:09 +0000

    check on initial config file

--- PM3/libs/common.py
+++ PM3/libs/common.py
@@ -1,10 +1,15 @@
 from configparser import ConfigParser
 from pathlib import Path
 import logging
+import PM3.model.errors as PM3_errors
 
 #pm3_home_dir = config['main_section'].get('pm3_home_dir')
 pm3_home_dir = Path('~/.pm3').expanduser()
-config_file = f'{pm3_home_dir}/config.ini'
+config_file = Path(f'{pm3_home_dir}/config.ini')
+
+if not config_file.is_file():
+    print(f"Config file not found at path {config_file}! Exiting...")
+    exit(PM3_errors.CONFIG_FILE_NOT_FOUND)
 
 config = ConfigParser()
 config.read(config_file)
